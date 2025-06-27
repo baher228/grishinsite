ee2addf445456648600a72fc1ba746905ce06920
baher228 <bah36937@gmail.com>
2025-06-28 00:20:06 +0300

    bs

--- server/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { protectMiddleware } from "@/middleware/protect.middleware";
-import { userProfile } from "../controllers/user.controller";
-import { Router } from "express";
-
-const router = Router();
-
-router.route("/profile").get(protectMiddleware, userProfile);
-
-export default router;
