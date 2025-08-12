c616e9316766ea69cd1d66c262669b3a0d1592a1
traher228 <bah36937@gmail.com>
2025-08-12 12:41:14 +0300

    Fix import paths in product controller and update TypeScript configuration

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
-import { getAllProducts, getProductsByCategory } from "@/services/getproduct";
+import { getAllProducts, getProductsByCategory } from "../services/getproduct";
 
 export const getCategory = asyncHandler(async (req: Request, res: Response) => {
-    const { category } = req.body;
-    console.log("Fetching products for category:", category);
-    const products = await getProductsByCategory(category);
-    res.json({ products: products });
-  });
-  
-  export const getAll = asyncHandler(async (req: Request, res: Response) => {
-    console.log("Fetching products:");
-    const products = await getAllProducts();
-    res.json({ products: products });
-  });
-  
+  const { category } = req.body;
+  console.log("Fetching products for category:", category);
+  const products = await getProductsByCategory(category);
+  res.json({ products: products });
+});
+
+export const getAll = asyncHandler(async (req: Request, res: Response) => {
+  console.log("Fetching products:");
+  const products = await getAllProducts();
+  res.json({ products: products });
+});
