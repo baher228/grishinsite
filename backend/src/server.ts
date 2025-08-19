7bd9f16ecac8ddedbac50adec57383f781a58cca
traher228 <bah36937@gmail.com>
2025-08-19 16:32:48 +0300

    Adding json parsing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,6 +12,8 @@ const app = express();
 const cors = require("cors");
 
 async function main() {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
   app.use(
     cors({
       origin: [
